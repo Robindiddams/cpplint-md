db939265787863cfdce802cb2f48ac4be03fb22e
Robin Diddams <robindiddams@gmail.com>
2019-02-04 21:08:55 -0500

    be pretty when no linter errors

--- main.py
+++ main.py
@@ -9,16 +9,24 @@ def main():
 """
     filename = sys.argv[1]
     file = open(filename, "r")
+    errors = False
     for line in file:
         data = re.findall("^(.*\:\d+):\s+(.*)\s+\[(.*)\]\s+\[(.*)\]", line)
         if (data):
+            errors = True
             data = data[0]
             filepath = data[0]
             error = data[1]
             # error_type = data[2]
             mdout += "| {} | {} |\n".format(filepath, error)
-    out = open("{}.md".format(filename), "w")
-    out.write(mdout)
+    postfix = ""
+    contents = """## No linter Errors :tada:
+    """
+    if (errors):
+        postfix= "_errors"
+        contents = mdout
+    out = open("{}{}.md".format(filename, postfix), "w")
+    out.write(contents)
     out.close()
 
 if __name__ == '__main__':
